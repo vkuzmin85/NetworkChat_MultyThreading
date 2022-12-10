4fec7da1ed651545c7fd5b130a083687f497570a
vkuzmin85 <vpkhome@gmail.com>
2022-12-10 19:03:16 +0300

    client modified

--- client/src/main/java/org/example/Client1Run.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.io.IOException;
-
-public class Client1Run {
-    public static void main(String[] args) throws IOException {
-        new Client();
-    }
-}
--- client/src/main/java/org/example/Client2Run.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.io.IOException;
-
-public class Client2Run {
-    public static void main(String[] args) throws IOException {
-        new Client();
-    }
-}
